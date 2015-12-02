d55d4d21a2006d5b9870c9b828c74c56aff2b172
Scott <sfrancis@misinteractive.com>
2015-12-01 19:17:01 -0500

    Added bower.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 
 var gulp=require('gulp');
 var sassdoc = require('sassdoc');
+var sass = require('gulp-sass');
+var REPO_NAME='sass-semantic-components';
 
 gulp.task('default',function(){
-    console.log('echo sass semantic components...');
+    console.log(REPO_NAME + ' ..."tasks: gulp dist|sassdoc"');
 });
 
 gulp.task('sassdoc', function () {
@@ -11,3 +13,15 @@ gulp.task('sassdoc', function () {
         .pipe(sassdoc());
 
 });
+
+gulp.task('dist', function () {
+    compileSass();
+});
+
+
+///private
+function compileSass(){
+    gulp.src('./src/semantic.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./dist'));
+}
